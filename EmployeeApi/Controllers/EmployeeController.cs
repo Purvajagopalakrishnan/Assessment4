55a23c0f1448cabc6ad054bc4e77da2fcee99652
purvaja gopalakrishnan <purvajag@CESCHN.COM>
2018-12-12 21:50:45 +0530

    Print

---
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+        
+    {
+        List<Employees> emp = new List<Employees>()
+            {
+                new Employees {Emp_Name = "Sam", Emp_Type = "Part-time"},
+                new Employees {Emp_Name = "Sowmiya", Emp_Type = "Part-time"},
+                new Employees {Emp_Name = "Nithya", Emp_Type = "Part-time"},
+                new Employees {Emp_Name = "John", Emp_Type = "Full-time"},
+                new Employees {Emp_Name = "Jim", Emp_Type = "Contractor"},
+                new Employees {Emp_Name = "Seam", Emp_Type = "Part-time"},
+                new Employees {Emp_Name = "Riya", Emp_Type = "Contractor"},
+                new Employees {Emp_Name = "Priya", Emp_Type = "Full-time"},
+                new Employees {Emp_Name = "Rahul", Emp_Type = "Part-time"},
+                new Employees {Emp_Name = "Ram", Emp_Type = "Full-time"},
+            };
+        public ActionResult<Dictionary<string, Employees>> GetDetails()
+        {
+           
+            return Ok(emp);
+        }
+       [HttpPost]
+        [Route("UpdateDetails")]
+        public void Post([FromBody] Employees e)
+        {
+            foreach (var employee in emp)
+            {
+                if(employee.Emp_Name == e.Emp_Name)
+                {
+                    employee.Emp_Type = e.Emp_Type;
+                }
+            }
+          //  return Ok();
+        }
+    }
+}
---
+++ EmployeeApi/Models/Employees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeApi.Models
+{
+    public class Employees
+    {
+        public string Emp_Name { get; set; }
+        public string Emp_Type { get; set; }
+    }
+
+}
